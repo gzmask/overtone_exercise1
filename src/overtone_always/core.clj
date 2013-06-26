0aa4234f79c7e3e2f2397e73741a0bd63ac9d581
gzmask <gzmask@gmail.com>
2013-06-26 13:45:08 -0600

    removed unessary parts

--- src/overtone_always/core.clj
+++ src/overtone_always/core.clj
@@ -3,84 +3,19 @@
 
 (boot-external-server)    ; boot an external server
 
-; learning my basic kicks
-(def kick (sample (freesound-path 2086)))
-
-(def one-twenty-bpm (metronome 120))
-
-(defn looper [nome sound]    
-    (let [beat (nome)]
-        (at (nome beat) (sound))
-        (apply-at (nome (inc beat)) looper nome sound [])))
-
-(looper one-twenty-bpm kick)
-
 ;sound synthesis. wave functions takes amplitude and frequency and outputs amplitude
 ;thus these functions are manapulating with the outputs, with are amplitudes.
-(definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
-  (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
-     (saw freq)
-     (saw (- freq 220))
-     vol))
-
 (definst sin-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
   (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
      (+ (sin-osc freq)
         (sin-osc (* freq 2))
         (sin-osc (* freq 4)))
      vol))
+
 (demo (sin-wave))
-(play-chord2 (chord :e4 :major) (now))
-
-(definst square-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
-  (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
-     (lf-pulse freq)
-    vol))
-
-
-(definst noisey [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
-  (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
-     (pink-noise) ; also have (white-noise) and others...
-     vol))
-
-(definst triangle-wave [freq 440 attack 0.01 sustain 0.1 release 0.4 vol 0.4] 
-  (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
-     (lf-tri freq)
-     vol))
-
-(definst spooky-house [freq 440 width 0.15 
-                         attack 0.3 sustain 4 release 0.3 
-                         vol 0.4] 
-  (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
-     (sin-osc (+ freq (* 20 (lf-pulse:kr 1.5 0 width))))
-     vol))
-
-(odoc lf-pulse:kr)
-;lf-pulse is a pulse oscillator. it generates signals such as 000000100000100000001, a.k.a pulse
-
-(spooky-house)
-;(saw-wave 440)
-;(saw-wave 523.25)
-;(saw-wave 261.62) ; This is C4
-;(saw-wave (midi->hz 69))
-;(saw-wave (midi->hz 72))
-;(saw-wave (midi->hz 60)) ; This is C4
-;(saw-wave (midi->hz (note :A4)))
-;(saw-wave (midi->hz (note :C5)))
-;(saw-wave (midi->hz (note :C4))) ; This is C4! Surprised?
-
-(defn note->hz [music-note]
-    (midi->hz (note music-note)))
-
 
 (defn wave [music-note]
     (sin-wave (midi->hz (note music-note))))
-
-(defn a []
-  (do 
-    (wave :A4)
-    (wave :C5)
-    (wave :C4)))
 
 (defn play-chord [a-chord]
   (doseq [note a-chord] (wave note)))
@@ -95,21 +30,7 @@
 (play-chord2 (chord :c4 :major) (now))
 (play-chord (chord :c4 :minor))
 
-(defn chord-progression-time []
-  (let [time (now)]
-    (at time (play-chord (chord :C4 :major)))
-    (at (+ 1000 time) (play-chord (chord :G3 :major)))
-    (at (+ 2000 time) (play-chord (chord :F3 :sus4)))
-    (at (+ 3300 time) (play-chord (chord :F3 :major)))
-    (at (+ 4000 time) (play-chord (chord :G3 :major)))))
-
 (def metro (metronome 200))
-(defn chord-progression-beat [m beat-num]
-  (at (m (+ 0 beat-num)) (play-chord (chord :C4 :major)))
-  (at (m (+ 4 beat-num)) (play-chord (chord :G3 :major)))
-  (at (m (+ 8 beat-num)) (play-chord (chord :A3 :minor)))
-  (at (m (+ 12 beat-num)) (play-chord (chord :F3 :major)))  
-)
 
 (defn play-scale [beats notes metro beat-num]
   (doseq [[beat note] (map list beats notes)]
@@ -176,5 +97,4 @@
     (at (m (+ 28 b)) (play-chord (chord :e3 :major)))
     (apply-at (m (+ 32 b)) ray-beat m (+ 32 b) []))
 (ray-beat metro (metro))
-(looper one-twenty-bpm kick)
 (stop)
