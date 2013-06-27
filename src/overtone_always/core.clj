770c5c7b49659835493da53adea1fb1efeb746ec
gzmask <gzmask@gmail.com>
2013-06-27 14:13:57 -0600

    working with leipzig

--- src/overtone_always/core.clj
+++ src/overtone_always/core.clj
@@ -1,103 +1,81 @@
 (ns overtone-always.core 
-    (:use [overtone.core]))
+    (:require [leipzig.melody :as lm]
+              [leipzig.scale :as ls]
+              [leipzig.canon :as lc]
+              [leipzig.live :as ll]
+              [overtone.live :as overtone]
+              [overtone.synth.stringed :as strings]))
 
-(boot-external-server)    ; boot an external server
+;(overtone/boot-external-server)     ;boot an external server
 
 ;sound synthesis. wave functions takes amplitude and frequency and outputs amplitude
 ;thus these functions are manapulating with the outputs, with are amplitudes.
-(definst sin-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
-  (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
-     (+ (sin-osc freq)
-        (sin-osc (* freq 4)))
+(overtone/definst sin-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
+  (* (overtone/env-gen (overtone/lin-env attack sustain release) 1 1 0 1 overtone/FREE)
+     (+ (overtone/sin-osc freq)
+        (overtone/sin-osc (* freq 4)))
      vol))
 
-(demo (sin-wave))
-; here, how I can read the fucking source code!
-(meta #'overtone.core/sin-osc)
-(meta #'overtone.core/saw)
+(strings/gen-stringed-synth ektara 1 (true))
 
-(defn wave [music-note]
-    (sin-wave (midi->hz (note music-note))))
+(overtone/odoc strings/gen-stringed-synth)
+(overtone/odoc lm/phrase)
+(overtone/odoc lm/where)
 
-(defn play-chord [a-chord]
-  (doseq [note a-chord] (wave note)))
-(odoc meta)
+(defn pick [distort amp {midi :pitch, start :time, length :duration}]
+  (let [synth-id (overtone/at start
+                              (ektara midi :distort distort :amp amp :gate 1))]
+    (overtone/at (+ start length) (overtone/ctl synth-id :gate 0))))
 
-;play-chord with delay
-(defn play-chord2 [a-chord time]
-      (doseq [[note timestamp] (map list a-chord [time (+ time 100) (+ time 200)])]
-             (at timestamp (wave note))))
-(map list (chord :c4 :major) [(now) (+ (now) 100) (+ (now) 200)])
-(chord :c4 :major)
-(chord :c4 :minor)
-(play-chord2 (chord :c4 :major) (now))
-(play-chord (chord :c4 :minor))
+;polyrism kinda functions
+(defmethod ll/play-note :leader [note]
+  (pick 0.7 1.0 note))
+(defmethod ll/play-note :follower [note]
+  (pick 0.3 1.0 note))
+(defmethod ll/play-note :bass [note]
+  (pick 0.9 0.2 (update-in note [:pitch] #(- % 12))))
 
-(def metro (metronome 200))
+(def melody "A simple melody built from durations and pitches."
+               ; Row, row, row your boat,
+  (->> (lm/phrase [3/3 3/3 2/3 1/3 3/3]
+               [  0   0   0   1   2])
+    (lm/then
+               ; Gently down the stream,
+       (lm/phrase [2/3 1/3 2/3 1/3 6/3]
+               [  2   1   2   3   4]))
+    (lm/then
+               ; Merrily, merrily, merrily, merrily,
+       (lm/phrase (repeat 12 1/3) 
+               (mapcat (partial repeat 3) [7 4 2 0])))
+    (lm/then
+               ; Life is but a dream!
+       (lm/phrase [2/3 1/3 2/3 1/3 6/3] 
+               [  4   3   2   1   0]))
+    (lm/where :part (lm/is :leader))))
 
-(defn play-scale [beats notes metro beat-num]
-  (doseq [[beat note] (map list beats notes)]
-    (at (metro (+ beat beat-num)) (play-chord2 (chord note :major) (metro (+ beat beat-num))))))
+(def bass "A bass part to accompany the melody."
+  (->> (lm/phrase [1  1 2]
+               [0 -3 0])
+     (lm/where :part (lm/is :bass))
+     (lm/times 4)))
 
-(play-scale [0    1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17   18   19  20  21  22  23  24  25  26  27  28  29]
-            [:c4 :c0 :g3 :a3 :b3 :b0 :a3 :g3 :g3 :e3 :e3 :g3 :g3 :b3 :a3 :g3 :g3 :f#3 :f#3 :g3 :g3 :a3 :a3 :b3 :b3 :g3 :g3 :e3 :e3 :e3]
-            metro
-            (metro))
+(defn row-row
+  "Play the tune 'Row, row, row your boat' as a round."
+  [speed key]
+  (->> melody
+    (lm/with bass)
+    (lm/times 2)
+    (lc/canon (comp (lc/simple 4)
+                 (partial lm/where :part (lm/is :follower))))
+    (lm/where :time speed)
+    (lm/where :duration speed)
+    (lm/where :pitch key)
+    ll/play))
 
-(defn play-scale1 [beats notes metro beat-num]
-    (doseq [[beat note] (map list (reductions + beats) notes)]
-      (let [time (metro (+ beat beat-num))] 
-        (at time (play-chord2 (chord note :major) time)))))
+(doc ls/minor)
 
-(play-scale1 [0   2   1    1   2   1   1   2   1   1   2   1    1    2    1  1   2   2   2   2]
-             [:c4 :g3 :a3 :b3 :a3 :g3 :e3 :e3 :g3 :b3 :a3 :g3 :f#3 :f#3 :g3 :a3 :b3 :g3 :e3 :e3]
-             metro
-             (metro))
+(row-row (lm/bpm 120) (comp ls/C ls/sharp ls/major))
 
-(odoc let)
-(= [[1 1] [2 2] [3 3]] (map list [1 2 3] [1 2 3]))
+(row-row (lm/bpm 90) (comp ls/low ls/B ls/flat ls/minor))
 
-(= '(1 2 3) [1 2 3])
-
-(defn play-scale2 [beat-notes metro beat-num]
-  (doseq [[beat note] beat-notes]
-    (at (metro (+ beat beat-num)) (play-chord (chord note :major))))
-)
-(play-scale2 [[0 :c4] [2 :g3] [3 :a3] [4 :b3] [6 :a3] [7 :g3]]
-             metro
-             (metro))
-
-(defn play-scale3 [beat-notes metro beat-num]
-  (doseq [[beat note] (partition 2 beat-notes)]
-    (at (metro (+ beat beat-num)) (play-chord2 (chord note :major) (metro (+ beat beat-num)))))
-)
-(play-scale3 [0 :c4 2 :g3 3 :a3 4 :b3 6 :a3 7 :g3 8 :e3 10 :e3 11 :g3 12 :b3 14 :a3 15 :g3 16 :f#3 18 :f#3 19 :g3 20 :a3 22 :b3 24 :g3 26 :e3 28 :e3]
-             metro
-             (metro))
-
-  
-
-(defn ray-beat [m b]
-    (at (m (+ 0 b)) (play-chord (chord :c4 :major)))
-    (at (m (+ 2 b)) (play-chord (chord :g3 :major)))
-    (at (m (+ 3 b)) (play-chord (chord :a3 :major)))
-    (at (m (+ 4 b)) (play-chord (chord :b3 :major)))
-    (at (m (+ 6 b)) (play-chord (chord :a3 :major)))
-    (at (m (+ 7 b)) (play-chord (chord :g3 :major)))
-    (at (m (+ 8 b)) (play-chord (chord :e3 :major)))
-    (at (m (+ 10 b)) (play-chord (chord :e3 :major)))
-    (at (m (+ 11 b)) (play-chord (chord :g3 :major)))
-    (at (m (+ 12 b)) (play-chord (chord :b3 :major)))
-    (at (m (+ 14 b)) (play-chord (chord :a3 :major)))
-    (at (m (+ 15 b)) (play-chord (chord :g3 :major)))
-    (at (m (+ 16 b)) (play-chord (chord :f#3 :major)))
-    (at (m (+ 18 b)) (play-chord (chord :f#3 :major)))
-    (at (m (+ 19 b)) (play-chord (chord :g3 :major)))
-    (at (m (+ 20 b)) (play-chord (chord :a3 :major)))
-    (at (m (+ 22 b)) (play-chord (chord :b3 :major)))
-    (at (m (+ 24 b)) (play-chord (chord :g3 :major)))
-    (at (m (+ 26 b)) (play-chord (chord :e3 :major)))
-    (at (m (+ 28 b)) (play-chord (chord :e3 :major)))
-    (apply-at (m (+ 32 b)) ray-beat m (+ 32 b) []))
-(ray-beat metro (metro))
-(stop)
+(overtone/stop)
